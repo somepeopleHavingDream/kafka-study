21f2c0d997e0050f22c062004d63148b4bc97b33
yangxin <1393003255@qq.com>
2021-01-13 14:16:47 +0800

    init: 初始化工程

---
+++ src/test/java/org/yangxin/kafka/kafkastudy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yangxin.kafka.kafkastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
