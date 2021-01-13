9c2c4f8d7b575a3aa405bb3621223715d1b69ca8
yangxin <1393003255@qq.com>
2021-01-13 15:00:30 +0800

    feat: 创建Topic演示

--- src/main/java/org/yangxin/kafka/kafkastudy/admin/AdminSample.java
+++ src/main/java/org/yangxin/kafka/kafkastudy/admin/AdminSample.java
@@ -3,7 +3,11 @@ package org.yangxin.kafka.kafkastudy.admin;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 /**
@@ -13,10 +17,13 @@ import java.util.Properties;
 @Slf4j
 public class AdminSample {
 
+    public static final String TOPIC_NAME = "kafka_topic";
+
     public static void main(String[] args) {
-        AdminClient adminClient = AdminSample.adminClient();
-        log.info("adminClient: [{}]", adminClient);
-//        System.out.printf("adminClient: [%s]%n", adminClient);
+//        AdminClient adminClient = AdminSample.adminClient();
+//        log.info("adminClient: [{}]", adminClient);
+
+        createTopic();
     }
 
     /**
@@ -28,5 +35,18 @@ public class AdminSample {
         properties.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.3.3:9092");
 
         return AdminClient.create(properties);
+    }
+
+    /**
+     * 创建Topic实例
+     */
+    public static void createTopic() {
+        AdminClient adminClient = adminClient();
+
+        // 副本因子
+        short replicationFactor = 1;
+        NewTopic newTopic = new NewTopic("", 1, replicationFactor);
+        CreateTopicsResult topics = adminClient.createTopics(Collections.singletonList(newTopic));
+        log.info("topics: [{}]", topics);
     }
 }
