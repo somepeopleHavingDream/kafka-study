662d79648786e8522f64e4b8db82c9b622f12513
yangxin <1393003255@qq.com>
2021-01-13 15:53:41 +0800

    feat: 查看Topic列表及Internal杂谈

--- src/main/java/org/yangxin/kafka/kafkastudy/admin/AdminSample.java
+++ src/main/java/org/yangxin/kafka/kafkastudy/admin/AdminSample.java
@@ -1,14 +1,12 @@
 package org.yangxin.kafka.kafkastudy.admin;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.KafkaFuture;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yangxin
@@ -19,11 +17,49 @@ public class AdminSample {
 
     public static final String TOPIC_NAME = "kafka_topic";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 //        AdminClient adminClient = AdminSample.adminClient();
 //        log.info("adminClient: [{}]", adminClient);
 
-        createTopic();
+//        createTopic();
+
+        // 获取Topic列表
+        listTopic();
+    }
+
+    /**
+     * 创建Topic实例
+     */
+    public static void createTopic() throws InterruptedException {
+        AdminClient adminClient = adminClient();
+
+        // 副本因子
+        short replicationFactor = 1;
+        NewTopic newTopic = new NewTopic(TOPIC_NAME, 1, replicationFactor);
+        CreateTopicsResult topics = adminClient.createTopics(Collections.singletonList(newTopic));
+
+        // adminClient.createTopics内部用了future机制，有可能子方法还未调用完毕，调用方法已经返回，导致topic创建失败
+        // 暂时找不到更好的方法，只能让调用方法休眠2秒，以等待子方法执行完毕
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    /**
+     * 获取Topic列表
+     */
+    public static void listTopic() throws ExecutionException, InterruptedException {
+        AdminClient adminClient = adminClient();
+        ListTopicsOptions listTopicsOptions = new ListTopicsOptions();
+        listTopicsOptions.listInternal(true);
+        ListTopicsResult listTopicsResult = adminClient.listTopics(listTopicsOptions);
+//        ListTopicsResult listTopicsResult = adminClient.listTopics();
+        Set<String> nameSet = listTopicsResult.names().get();
+        Collection<TopicListing> topicListingCollection = listTopicsResult.listings().get();
+
+        // 打印names
+        nameSet.forEach(System.out::println);
+        // 打印topicListingCollection
+        topicListingCollection.forEach(System.out::println);
+
     }
 
     /**
@@ -35,18 +71,5 @@ public class AdminSample {
         properties.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.3.3:9092");
 
         return AdminClient.create(properties);
-    }
-
-    /**
-     * 创建Topic实例
-     */
-    public static void createTopic() {
-        AdminClient adminClient = adminClient();
-
-        // 副本因子
-        short replicationFactor = 1;
-        NewTopic newTopic = new NewTopic("", 1, replicationFactor);
-        CreateTopicsResult topics = adminClient.createTopics(Collections.singletonList(newTopic));
-        log.info("topics: [{}]", topics);
     }
 }
