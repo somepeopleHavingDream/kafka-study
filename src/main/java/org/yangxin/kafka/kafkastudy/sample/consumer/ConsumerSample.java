cda0217b3373fdd21af56939f14c715db5a14f93
yangxin <1393003255@qq.com>
2025-10-18 00:04:00 +0800

    feat: Kafka 核心 API —— Consumer ，Consumer 手动控制一到多个分区

--- src/main/java/org/yangxin/kafka/kafkastudy/sample/consumer/ConsumerSample.java
+++ src/main/java/org/yangxin/kafka/kafkastudy/sample/consumer/ConsumerSample.java
@@ -19,7 +19,9 @@ public class ConsumerSample {
         // 手动提交 offset
 //        commitedOffset();
         // 手动对每个 partition 进行提交
-        commitedOffsetWithPartition();
+//        commitedOffsetWithPartition();
+        // 手动订阅某个或某些分区，并提交 offset
+        commitedOffsetWithPartition2();
     }
 
     private static void helloWorld() {
@@ -87,6 +89,49 @@ public class ConsumerSample {
         try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
             // 消费订阅哪一个 topic 或者几个 topic
             consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
+                // 每个 partition 单独处理
+                for (TopicPartition partition : records.partitions()) {
+                    List<ConsumerRecord<String, String>> pRecord = records.records(partition);
+                    for (ConsumerRecord<String, String> record : pRecord) {
+                        System.out.printf("partition = %d offset = %d, key = %s, value = %s%n", record.partition(),
+                                record.offset(), record.key(), record.value());
+                    }
+
+                    long lastOffset = pRecord.get(pRecord.size() - 1).offset();
+                    // 单个 partition 中的 offset ，并且进行提交
+                    Map<TopicPartition, OffsetAndMetadata> offset = new HashMap<>();
+                    offset.put(partition, new OffsetAndMetadata(lastOffset + 1));
+                    // 提交 offset
+                    consumer.commitSync(offset);
+
+                    System.out.println("partition = " + partition);
+                }
+            }
+        }
+    }
+
+    /**
+     * 手动提交 offset，并且手动控制 partition，更高级
+     */
+    private static void commitedOffsetWithPartition2() {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "false");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            // 0,1 两个 partition
+            TopicPartition p0 = new TopicPartition(TOPIC_NAME, 0);
+            TopicPartition p1 = new TopicPartition(TOPIC_NAME, 1);
+
+            // 消费订阅某个 Topic 的某个分区
+            consumer.assign(Collections.singletonList(p0));
+
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
                 // 每个 partition 单独处理
