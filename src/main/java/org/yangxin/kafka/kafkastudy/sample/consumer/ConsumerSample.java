3878af575de24e99a7bde883f7167d00d15390c6
yangxin <1393003255@qq.com>
2025-10-17 23:28:40 +0800

    feat: Kafka 核心 API —— Consumer ，Consumer 之手动提交

--- src/main/java/org/yangxin/kafka/kafkastudy/sample/consumer/ConsumerSample.java
+++ src/main/java/org/yangxin/kafka/kafkastudy/sample/consumer/ConsumerSample.java
@@ -13,7 +13,8 @@ public class ConsumerSample {
     private final static String TOPIC_NAME = "kafka_topic";
 
     public static void main(String[] args) {
-        helloWorld();
+//        helloWorld();
+        commitedOffset();
     }
 
     private static void helloWorld() {
@@ -34,6 +35,31 @@ public class ConsumerSample {
                     System.out.printf("partition = %d offset = %d, key = %s, value = %s%n", record.partition(),
                             record.offset(), record.key(), record.value());
                 }
+            }
+        }
+    }
+
+    private static void commitedOffset() {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "false");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            // 消费订阅哪一个 topic 或者几个 topic
+            consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("partition = %d offset = %d, key = %s, value = %s%n", record.partition(),
+                            record.offset(), record.key(), record.value());
+                }
+
+                // 手动通知 offset 提交
+                consumer.commitAsync();
             }
         }
     }
