010776bd1f7e10aaca12286ee7b2c5151a73c1b4
yangxin <1393003255@qq.com>
2025-10-18 21:56:42 +0800

    feat: Kafka 核心 API —— Consumer ，Consumer 控制 offset 起始位置

--- src/main/java/org/yangxin/kafka/kafkastudy/sample/consumer/ConsumerSample.java
+++ src/main/java/org/yangxin/kafka/kafkastudy/sample/consumer/ConsumerSample.java
@@ -21,7 +21,9 @@ public class ConsumerSample {
         // 手动对每个 partition 进行提交
 //        commitedOffsetWithPartition();
         // 手动订阅某个或某些分区，并提交 offset
-        commitedOffsetWithPartition2();
+//        commitedOffsetWithPartition2();
+        // 手动指定 offset 的起始位置，及手动提交 offset
+        controlOffset();
     }
 
     private static void helloWorld() {
@@ -133,6 +135,65 @@ public class ConsumerSample {
             consumer.assign(Collections.singletonList(p0));
 
             while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
+                // 每个 partition 单独处理
+                for (TopicPartition partition : records.partitions()) {
+                    List<ConsumerRecord<String, String>> pRecord = records.records(partition);
+                    for (ConsumerRecord<String, String> record : pRecord) {
+                        System.out.printf("partition = %d offset = %d, key = %s, value = %s%n", record.partition(),
+                                record.offset(), record.key(), record.value());
+                    }
+
+                    long lastOffset = pRecord.get(pRecord.size() - 1).offset();
+                    // 单个 partition 中的 offset ，并且进行提交
+                    Map<TopicPartition, OffsetAndMetadata> offset = new HashMap<>();
+                    offset.put(partition, new OffsetAndMetadata(lastOffset + 1));
+                    // 提交 offset
+                    consumer.commitSync(offset);
+
+                    System.out.println("partition = " + partition);
+                }
+            }
+        }
+    }
+
+    /**
+     * 手动提交 offset 的起始位置，及手动提交 offset
+     */
+    private static void controlOffset() {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "false");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            // 0,1 两个 partition
+            TopicPartition p0 = new TopicPartition(TOPIC_NAME, 0);
+
+            // 消费订阅某个 Topic 的某个分区
+            consumer.assign(Collections.singletonList(p0));
+
+            while (true) {
+                // 手动指定 offset 指定位置
+
+                /*
+                    1 人为控制 offset 起始位置
+                    2 如果出现程序错误，重复消费一次
+                 */
+
+                /*
+                    1 第一次从 0 消费【一般情况】
+                    2 比如一次消费了 100 条，offset 置为 101 并且存入 redis
+                    3 每次 poll 之前，从 redis 中获取最新的 offset 位置
+                    4 每次从这个位置开始消费
+                 */
+
+                // 手动指定 offset 起始位置
+                consumer.seek(p0, 400);
+
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
                 // 每个 partition 单独处理
                 for (TopicPartition partition : records.partitions()) {
