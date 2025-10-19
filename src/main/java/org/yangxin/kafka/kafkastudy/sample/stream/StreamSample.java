ada047e3ced308ffe4b8967715813e5f42b6fcff
yangxin <1393003255@qq.com>
2025-10-19 16:39:07 +0800

    feat: Kafka 核心 API —— Stream，Kafka Stream 演示准备

---
+++ src/main/java/org/yangxin/kafka/kafkastudy/sample/stream/StreamSample.java
@@ -0,0 +1,11 @@
+package org.yangxin.kafka.kafkastudy.sample.stream;
+
+@SuppressWarnings("unused")
+public class StreamSample {
+    private static final String INPUT_TOPIC = "stream-in";
+    private static final String OUTPUT_TOPIC = "stream-out";
+
+    public static void main(String[] args) {
+
+    }
+}
